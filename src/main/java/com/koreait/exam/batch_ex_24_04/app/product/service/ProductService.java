8d401feb1e22a59c1317b5e2c47e6f149d95d7cb
Gouiseong <rhdmltjd1324@gmail.com>
2024-04-19 15:29:56 +0900

    도매가 추가, 노출용 색상, 사이즈 추가

--- src/main/java/com/koreait/exam/batch_ex_24_04/app/product/service/ProductService.java
+++ src/main/java/com/koreait/exam/batch_ex_24_04/app/product/service/ProductService.java
@@ -14,13 +14,14 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
-    public Product create(String name, int price, String makerShopName, List<ProductOption> options) {
+    public Product create(String name, int price, int wholesalePrice, String makerShopName, List<ProductOption> options) {
         Product product = Product.builder()
                 .name(name)
                 .price(price)
+                .wholesalePrice(wholesalePrice)
                 .makerShopName(makerShopName).build();
 
-        for(ProductOption option : options) {
+        for (ProductOption option : options) {
             product.addOption(option);
         }
 
